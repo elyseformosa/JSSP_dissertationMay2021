aac19c25883c4a39a5ad8bdeb7cb1ea5077911ec
elyseformosa <83924773+elyseformosa@users.noreply.github.com>
2021-05-15 15:52:57 +0200

    Delete Genetic Algorithm directory

--- Genetic Algorithm/RKmutation.m
+++
@@ -1,7 +0,0 @@
-function child = RKmutation(Parent,index,n,m)
-
-Parent([index(1) index(2)]) = Parent([index(2) index(1)]);
-child=Parent;
-
-end
-
--- Genetic Algorithm/mutationjobbased.m
+++
@@ -1,9 +0,0 @@
-function child = mutationjobbased(Parent,n,m)
-
-Parent=unique(Parent,'stable');      %gives unique values in their original order
-
-index = randperm(n,2);   %finds two random numbers
-
-Parent([index(1) index(2)]) = Parent([index(2) index(1)]);
-child=Parent(repelem(1:n,1,m));    %repeat n jobs for m times ex. [4 4 4 3 3 3 2 2 2 1 1 1]
-end
